62bbc8d00a903cca9e945483c3cc5d76f1b0380d
Yuneska <Ineskastuardeska@gmail.com>
2022-03-10 02:03:54 +0200

    done_class_work_5

--- lesson 5/cw5.js
+++ lesson 5/cw5.js
@@ -1,135 +1,129 @@
+// Всі функції повинні бути описані стрілочним типом!!!!
 // 1 Створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
-function min(a, b, c) {
+const min = (a, b, c) => {
     if (a < b && a < c) {
-        console.log(a)
+        console.log(a);
+        document.write(a);
     } else if (b < a && b < c) {
-        console.log(b)
+        console.log(b);
+        document.write(b);
     } else {
-        console.log(c)
+        console.log(c);
+        document.write(c)
     }
-}
-
-min(20, 50, 40)
+};
+min(28, 54, 35)
 
 // 2 Створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
-function max(a, b, c) {
+let max = (a, b, c) => {
     if (a > b && a > c) {
-        console.log(a)
+        console.log(a);
+        document.write(a)
     } else if (b > a && b > c) {
-        console.log(b)
+        console.log(b);
+        document.write(b)
     } else {
-        console.log(c)
+        console.log(c);
+        document.write(c)
     }
-}
-
-max(30, 70, 50)
+};
+max(65, 65, 98)
 
 // 3 Створити функцію яка повертає найбільше число з масиву
-let arr = [110, 200, 350, 440, 50];
-
-function maxNum(array) {
-    let max = array[0];
+let arr = [32, 76, 47, 29, 90, 54];
+const bigNum = (array) => {
+    let max = array[0]
     for (const element of array) {
         if (element > max) {
-            max = element;
+            max = element
         }
     }
-    return max;
+    return max
 }
-
-{
-    console.log('Максимальне число в масиві:',maxNum(arr))
-}
-
-document.write('Максимальне число в масиві:',maxNum(arr));
+document.write(` Найбільше число масиву : ${bigNum(arr)}`)
 
 // 4 Створити функцію яка повертає найменьше число з масиву
-function minNum(array) {
-    let min = array [0];
+let arr2 = [98, 677, 54, 56, 34, 24562, 173, 39];
+const smallNum = (array) => {
+    let min = array[0];
     for (const element of array) {
         if (element < min) {
-            min = element;
+            min = element
         }
     }
-    return min;
+    return min
 }
-
-{
-    console.log('Мінімальне число в масиві:',minNum(arr))
-}
-
-document.write('Мінімальне число в масиві:',minNum(arr))
+document.write(` Найменше число масиву : ${smallNum(arr2)}`)
 
 // 5 Створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад [1,2,10]->13
-let numbers = [33, 45, 64, 32, 61];
-
-function sum(array) {
-    let result = 0;
+let arrNum = [5, 4, 3, 2, 1];
+const sumNum = (array) => {
+    let sum = 0;
     for (const element of array) {
-        result = result + element
+        sum += element
     }
-    return result
+    return sum
 }
-
-{
-    console.log('Сума чисел в масиві:',sum(numbers))
-}
+document.write(sumNum(arrNum))
 
 // 6 Створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-function average(array) {
-    let result = 0;
+const average = (array) => {
+    let sum = 0;
     for (const element of array) {
-        result += element
+        sum += element
     }
-    return result / array.length
+    return sum / array.length
 }
-
-{
-    console.log('Середнє арифметичне число в масиві:', average(numbers))
-}
+document.write(average(arrNum))
 
 // 7 Створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
 // (Math використовувати заборонено);
-function maxMin(...arguments) {
+const maxMin = (...arguments) => {
     let max = arguments[0];
     let min = arguments[0];
     for (const element of arguments) {
         if (element > max) {
-            max = element;
+            max = element
         }
         if (element < min) {
-            min = element;
-        }}
-        console.log('Максимальне число:', max);
-        return min;
+            min = element
+        }
     }
-    document.write('Мінімальне число:'+ maxMin(44, 33, 88, 99, 77, 11));
+    console.log("Найбільше число :", max);
+    return min
+}
+document.write(' Найменше число : ' + maxMin(3, 5, 44, 6, 2, 45, 23, -354))
 
 // 8 Створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100))
 // та виводить його.
-function domRandom (length){
-let aar = [];
-for (let i=0; i<length; i++){aar.push(Math.random()*100)}
-return aar}
-document.write('Рандомні числа:',domRandom(7))
-
-
+const random = (length) => {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(Math.round(Math.random() * 100))
+    }
+    return arr
+}
+console.log('Рандомні числа : ', random(5))
 
 // 9 Створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit.
 // limit - аргумент, який характеризує кінцеве значення діапазону.
-function ranRandom (length, limit){
-    let ary=[];
-    for (let i=0; i<length; i++){ary.push(Math.round(Math.random()*limit))}
-    return ary}
-document.write('Лімітовані рандомні числа:',ranRandom(3,5))
+const random2 = (length, limit) => {
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+        arr.push(Math.round(Math.random() * limit))
+    }
+    return arr
+}
+document.write(random2(3, 50))
 
 // 10 Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
-function reverse(array){
-    let araNew =[];
-    for (let i=array.length-1, j=0; i>=0; i--, j++)
-    { araNew[j]=array[i];}
-    return araNew;
+const reverse = (array) => {
+    let arrRev = [];
+    for (let i = arr.length - 1, j = 0; i>= 0; i--, j++) {
+        arrRev[j] = array[i]
+    }
+    return arrRev
 }
-console.log(reverse([13,23,33]))
+console.log('Реверсивний масив : ',reverse([8,7,6,5]))
