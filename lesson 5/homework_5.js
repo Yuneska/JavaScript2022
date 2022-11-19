d19b23e1873d6d3ad122b5045910adcd09a7e674
Yuneska <Ineskastuardeska@gmail.com>
2022-11-19 22:47:01 +0200

    homework_5.js

--- lesson 5/homework_5.js
+++ lesson 5/homework_5.js
@@ -1,9 +1,5 @@
 // 1) Cтворити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б.
-function area(a, b) {
-    let s = a * b;
-    return s;
-}
-
+let area = (a, b) => a * b;
 console.log(area(2, 5))
 document.write('Площа прямокутника =', area(2, 5));
 
@@ -15,7 +11,12 @@ function circle(r) {
 }
 
 console.log(circle(5))
-document.write('Площа кола =', circle(3))
+document.write('Площа кола =', circle(3));
+
+let circle1 = (r) => Math.PI * Math.pow(r, 2);
+
+console.log(circle1(3));
+document.write('Площа кола = ', Math.round(circle1(3)))
 
 // 3) Cтворити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r.
 function cylinder(h, r) {
@@ -25,6 +26,12 @@ function cylinder(h, r) {
 
 console.log(cylinder(3, 4));
 document.write('Площа циліндра =', cylinder(4, 3))
+
+
+let cilinder = (h, r) => 2 * Math.PI * h * r;
+
+console.log(cilinder(2, 3));
+document.write('Площа циліндра = ', Math.round(cilinder(2, 3)));
 
 // 4) Cтворити функцію яка приймає масив та виводить кожен його елемент.
 let mass = [1, 22, 333, 4444];
@@ -36,31 +43,40 @@ function array(arr) {
 
 array(mass)
 
+
+function array1(arr) {
+    for (const item of arr) {
+        console.log(item)
+    }
+}
+
+array1(mass)
+
 // 5) Cтворити функцію яка створює параграф з текстом. Текст задати через аргумент.
-function paragraph(arguments) {
-    document.write(`<p>${arguments}</p>`)
+function paragraph(text) {
+    document.write(`<p>${text}</p>`)
 }
 
 paragraph('Параграф з текстом')
 
 // 6) Cтворити функцію яка створює ul з трьома елементами li.
 // Текст li задати через аргумент всім однаковий.
-let list = (argument) => {
-    document.write(`<ul>`);
-    document.write(`<li>${argument}</li>`);
-    document.write(`<li>${argument}</li>`);
-    document.write(`<li>${argument}</li>`);
-    document.write(`</ul>`)
+let list = (text) => {
+    document.write(`<ul>
+    <li>${text}</li>
+    <li>${text}</li>
+    <li>${text}</li>
+    </ul>`)
 }
 list('Однаковий аргумент')
 
 // 7) Cтворити функцію яка створює ul з трьома елементами li.
 // Текст li задати через аргумент всім однаковий.
 // Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл).
-let list2 = (argument, counter) => {
+let list2 = (text, counter) => {
     document.write(`<ul>`);
     for (let i = 0; i < counter; i++) {
-        document.write(`<li>${argument}</li>`)
+        document.write(`<li>${text}</li>`)
     }
     document.write(`</ul>`)
 }
@@ -102,13 +118,12 @@ let minNum = (arr) => {
 
 minNum(numbers)
 
-
 // 11) Cтворити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
 // Приклад [1,2,10]->13
 let suma = (arr) => {
     let sum = 0;
     for (const element of arr) {
-        sum += element
+        sum += element //sum=sum+element
     }
     return sum
 }
