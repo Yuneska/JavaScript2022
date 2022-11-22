74b6f010d5cafd31aa80769e26891f9c889177e7
Yuneska <Ineskastuardeska@gmail.com>
2022-11-23 00:27:10 +0200

    lection_6

---
+++ lesson 6/lection_6.js
@@ -0,0 +1,58 @@
+// let str='hello okten';
+// console.log(str[0]);
+// let toUpperCase=str.toUpperCase();
+// console.log(toUpperCase);
+// console.log(toUpperCase.toLowerCase());
+// console.log(str.startsWith('hello'));
+// console.log(str.endsWith('en'));
+//
+// let substring=str.substring(0,7);
+// console.log(substring);
+//
+// let indexOfE=str.indexOf('e');
+// console.log(indexOfE);
+// let indexOfT=str.indexOf('t');
+// console.log(indexOfT);
+// console.log(str.substring(indexOfE, indexOfT));
+// console.log(str.charAt(7));
+//
+// let phone = '(067)292-88-92';
+// let replaceAll=phone.replaceAll('(', '');
+// console.log(replaceAll);
+// console.log(phone.replaceAll(')', '')
+//     .replaceAll('-', '')
+//     .replaceAll('0','1')
+// )
+// phone.replaceAll(/[)(-+)]/g,'') // global change /[]/g
+//
+// let obj='name:vasya,age:31';
+// let split=obj.split(',')
+// console.log(split);
+// let tupleName= split[0];
+// let nameValue=tupleName.split(':')
+// console.log(nameValue[0]);
+// let tupleAge=split[1];
+
+Array.isArray([]); //true
+Array.isArray({}); //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
